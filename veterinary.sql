df1cfd753c7b436188bc19b7db7ad422cd3930d3
Valerie Kristine Taberdo <taberdovk@gmail.com>
2025-11-30 22:39:49 +0800

    feat/create-table-owners

--- veterinary.sql
+++ veterinary.sql
@@ -1,1 +1,8 @@
-content here
+CREATE TABLE owners(
+    owenerid INT PRIMARY KEY,
+    ofirstname VARCHAR(50),
+    olastname VARCHAR(50),
+    address VARCHAR(100),
+    phone VARCHAR(15),
+    email VARCHAR(100),
+)
